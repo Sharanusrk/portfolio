d1e8130d2096f34ef4a5720475766f158399a413
sharanu <sharankumusagi@gmail.com>
2025-07-08 18:20:41 +0530

    Fix base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],// Base path for GitHub Pages
-  base: process.env.VITE_BASE_PATH || "/sharanurkportfolio/",
-
-})
+  plugins: [react()],
+  base: "/", // ✅ Required for Vercel
+});
